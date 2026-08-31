16d1b948faa39798ff26dee08e65786137a01175
sirkingbinx <bendawsdev@gmail.com>
2026-08-31 09:06:35 -0500

    Prevent patching on incompatible versions Only works on the plugin side, I need to implement the same thing for the patcher later

--- Translatilla.Plugin/Constants.cs
+++ Translatilla.Plugin/Constants.cs
@@ -5,4 +5,7 @@ public static class Constants
     public const string Name = "Translatilla";
     public const string Guid = "bingus.translatilla";
     public const string Version = "1.0.0";
+
+    public const string GLVersion = "1.0.3";
+    public const string UtillaVersion = "1.7.1";
 }
--- Translatilla.Plugin/Plugin.cs
+++ Translatilla.Plugin/Plugin.cs
@@ -1,4 +1,5 @@
 using BepInEx;
+using BepInEx.Bootstrap;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using UnityEngine;
@@ -25,12 +26,33 @@ public class Plugin : BaseUnityPlugin
 
     private void Awake()
     {
+        Logger = base.Logger;
+
         masterLibrary = Config.Bind(
             "Patching", "MasterLibrary",
             MasterLibrary.Utilla,
             "The library that handles modded functions."
         );
 
-        Logger = base.Logger;
+        var glInfo = Chainloader.PluginInfos["dev.gorillalibrary"].Metadata;
+        var utInfo = Chainloader.PluginInfos["org.legoandmars.gorillatag.utilla"].Metadata;
+
+        if (glInfo.Version != Constants.GLVersion) {
+            Logger.LogError("GorillaLibrary has not been patched for the latest version; initialization has been cancelled.");
+            Logger.LogError($"GL patch version: {Constants.GLVersion} ; GL version: {glInfo.Version}");
+            return;
+        }
+
+        if (utInfo.Version != Constants.UtillaVersion) {
+            Logger.LogError("Utilla has not been patched for the latest version; initialization has been cancelled.");
+            Logger.LogError($"Utilla patch version: {Constants.UtillaVersion} ; Utilla version: {utInfo.Version}");
+            return;
+        }
+
+        bool glMaster = masterLibrary.Value == MasterLibrary.GorillaLibrary;
+        bool utMaster = masterLibrary.Value == MasterLibrary.Utilla;
+
+        GorillaLibraryPatches.Apply(glMaster);
+        UtillaPatches.Apply(utMaster);
     }
 }
